75286e4d5b83df9e81d23650435eb0f016fb8ea8
kuberlog <collinalexbell@gmail.com>
2021-11-28 03:41:36 +0000

    init Treelon.cpp

---
+++ Treelon.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string get_tree_ascii() {
+  string tree = 
+
+"                                  # #### ####                             \n"
+"                                ### \/#|### |/####                        \n"
+"                               ##\/#/ \||/##/_/##/_#                      \n"
+"                             ###  \/###|/ \/ # ###                        \n"
+"                           ##_\_#\_\## | #/###_/_####                     \n"
+"                          ## #### # \ #| /  #### ##/##                    \n"
+"                           __#_--###`  |{,###---###-~                     \n"
+"                                     \ }{                                 \n"
+"                                      }}{                                 \n"
+"                                      }}{                                 \n"
+"                                 ejm  {{}                                 \n"
+"                                , -=-~{ .-^- _                            \n"
+"                                      `}                                  \n"
+"                                       {                                  \n";
+  return tree;
+}
+
+int main() {
+  cout << get_tree_ascii();
+  cout << endl << endl << "Welcome to Treelon" << endl;
+}
