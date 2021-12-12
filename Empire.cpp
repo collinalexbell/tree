744054269a8954d5c2017cf4ee5fefb39d204108
kuberlog <collinalexbell@gmail.com>
2021-12-12 11:55:23 +0000

    Empire.cpp can print a vehicle

--- Empire.cpp
+++ Empire.cpp
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 
 int main() {
+  int choice;
   cout << "Empire" << endl;
+  cout << "   1) Vehicles" << endl;
+  cin >> choice;
+  if(choice == 1){
+    auto fs = fstream("vehicle.txt", fstream::in | fstream::out);
+    char txt[1024];
+    while(!fs.eof()) {
+      fs.getline(txt, 1024);
+      cout << txt << endl;
+    }
+    fs.close();
+  } else {
+    cout << "choose a valid option" << endl;
+    main();
+  }
 }
